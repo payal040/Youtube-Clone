dff3f8517f12c8a4841174fc87fa095f1f161782
payal040 <payals5006@gmail.com>
2024-10-12 23:05:08 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,16 @@
+export const API_KEY = "AIzaSyB3RB5QUB09BSPNIsrsRMviF_wfoZAtFzU";   
+
+export const value_converter = (value) => {
+    if(value>=1000000)
+    {
+        return Math.floor(value/1000000)+"M";
+    }
+    else if(value>=1000)
+    {
+        return Math.floor(value/1000)+"K";
+    }
+    else
+    {
+        return value;
+    }
+}
